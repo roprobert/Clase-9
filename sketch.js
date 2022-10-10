df5dfdabd759d5e4f8d16fd4e895d435134aef98
roprobert <115504053+roprobert@users.noreply.github.com>
2022-10-10 15:35:40 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite (200,200,30,30);
+}
+
+function draw() 
+{
+  background(30);
+  if (keyDown("right")){
+    box.position.x = box.position.x + 5 ;
+    }
+  if (keyDown("left")){
+    box.position.x = box.position.x -5 ;
+  }
+  if (keyDown("up")){
+    box.position.y = box.position.y -5 ;
+  }
+  if (keyDown("down")){
+    box.position.y = box.position.y + 5 ;
+  }
+  drawSprites ();
+  }
+
+
+
+
+
+
